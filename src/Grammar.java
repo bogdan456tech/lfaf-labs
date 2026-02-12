a97f1bc1f50729b24b26977098241eb60270c828
Bogdan Arama <bogdan456ion@gmail.com>
2026-02-12 11:26:53 +0200

    Implement word generator and converter of regular grammar to finite automaton

---
+++ src/Grammar.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+public class Grammar {
+
+    private List<String> vn = new ArrayList<String>();
+    private List<String> vt = new ArrayList<String>();
+    private Map<String, List<String>> production = new HashMap<>();
+
+    public Grammar(List<String> vn, List<String> vt, Map<String, List<String>> production) {
+        this.vn.addAll(vn);
+        this.vt.addAll(vt);
+        this.production.putAll(production);
+    }
+
+    public String generateString() {
+
+        Random random = new Random();
+        String init = "S";
+        int r = random.nextInt(0,getList(init).size());
+        StringBuilder result = new StringBuilder(getList(init).get(r));
+        init=getList(init).get(r).substring(1,2);
+
+        while(true) {
+            r = random.nextInt(0,getList(init).size());
+
+            if(getList(init).get(r).length()==1) {
+                result.replace(result.length()-1,result.length(),getList(init).get(r));
+                break;
+            } else {
+                result.replace(result.length()-1,result.length()+1,getList(init).get(r));
+            }
+            init=getList(init).get(r).substring(1,2);
+        }
+
+        return result.toString();
+
+    }
+
+    public List<String> getList(String key) {
+        return production.get(key);
+    }
+
+    public FiniteAutomaton toFiniteAutomaton() {
+
+        Map<String,Integer> inputs = new HashMap<>(Map.of("a",0,"b",1,"c",2,"d",3,"e",4,"f",5));
+
+        List<List<String>> transitions = new ArrayList<>();
+
+        for(int i=0; i<vn.size()+1; i++) {
+            List<String> row = new ArrayList<>();
+            for(int j=0; j<vt.size(); j++) {
+                row.add("-");
+            }
+            transitions.add(row);
+        }
+
+        List<String> s = getList("S");
+        List<String> p = getList("P");
+        List<String> q = getList("Q");
+
+
+        for(int i=0; i<s.size(); i++) {
+            transitions.get(0).set(inputs.get(s.get(i).substring(0,1)),s.get(i).substring(1,2));
+        }
+
+        for(int i=0; i<p.size(); i++) {
+            if(p.get(i).length()==2) {
+                transitions.get(1).set(inputs.get(p.get(i).substring(0,1)),p.get(i).substring(1,2));
+            } else {
+                transitions.get(1).set(inputs.get(p.get(i).substring(0,1)),"X");
+            }
+        }
+
+        for(int i=0; i<q.size(); i++) {
+            if(q.get(i).length()==2) {
+                transitions.get(2).set(inputs.get(q.get(i).substring(0,1)),q.get(i).substring(1,2));
+            } else {
+                transitions.get(2).set(inputs.get(q.get(i).substring(0,1)),"X");
+            }
+        }
+
+        List<String> copyVn = new ArrayList<>(vn);
+        copyVn.add("X");
+
+        FiniteAutomaton fa = new FiniteAutomaton(
+                copyVn,
+                vt,
+                "S",
+                "X",
+                transitions
+        );
+
+        return fa;
+
+    }
+
+}
