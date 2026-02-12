64818dd1659b599c5d5bdb3a2e1b9616c2049147
Bogdan Arama <bogdan456ion@gmail.com>
2026-02-12 11:26:06 +0200

    Add Main java file

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Grammar grammar = new Grammar(
+                List.of("S", "P", "Q"),
+                List.of("a", "b", "c", "d", "e", "f"),
+                Map.of(
+                        "S", List.of("aP", "bQ"),
+                        "P", List.of("bP", "cP", "dQ", "e"),
+                        "Q", List.of("eQ", "fQ", "a")
+                )
+        );
+
+        System.out.println("5 words:");
+        for(int i=0; i<5; i++) {
+            System.out.println(grammar.generateString());
+        }
+
+        FiniteAutomaton fa = grammar.toFiniteAutomaton();
+
+        System.out.println(fa);
+
+        System.out.println(fa.checkWord("ae"));
+
+    }
+}
