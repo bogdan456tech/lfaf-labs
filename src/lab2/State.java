92b706e57dff7531f8d48330273aba68c0c9c15b
Bogdan Arama <bogdan456ion@gmail.com>
2026-03-19 11:10:09 +0200

    Add State class for the encapsulation of the of node

---
+++ src/lab2/State.java
@@ -0,0 +1,29 @@
+package lab2;
+
+import java.util.Objects;
+
+public class State {
+    String name;
+
+    public State(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof State)) return false;
+        State state = (State) o;
+        return name.equals(state.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
